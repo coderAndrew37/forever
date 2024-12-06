3c9962b001580f991c27dec11c597b5ad3cd0df3
coderAndrew37 <omolloandrew37@gmail.com>
2024-12-06 16:53:26 +0300

    changing the base url

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -1,4 +1,4 @@
 const isProduction = window.location.hostname !== "localhost";
 export const baseUrl = isProduction
-  ? "https://forever-svro.onrender.com/"
+  ? "https://forever-svro.onrender.com"
   : "http://localhost:5000";
