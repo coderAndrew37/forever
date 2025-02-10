6c887a66ad39077ad17bb7ccfd5bc9b033dfcfde
= <=>
2025-02-10 12:23:40 +0300

    update log entries for server and MongoDB connection details

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -1,8 +1,4 @@
 const isProduction = window.location.hostname !== "localhost";
 export const baseUrl = isProduction
-<<<<<<< HEAD
-  ? "https://forever-1-3cbm.onrender.com"
-=======
   ? "https://forever-tau.vercel.app"
->>>>>>> cfd292623decfa019c4565f110864eda0c817787
   : "http://localhost:5000";
