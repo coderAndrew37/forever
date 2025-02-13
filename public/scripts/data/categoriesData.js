a73b0b20d73105710a4673151b4bcae0b909beb2
= <=>
2025-02-13 20:58:05 +0300

    updating the category data

--- public/scripts/data/categoriesData.js
+++ public/scripts/data/categoriesData.js
@@ -1,23 +1,32 @@
 export const categories = [
   {
     slug: "drinks",
-    name: "Nutritional Drinks",
+    name: "Drinks",
     image: "/images/categories/drinks.jpg",
+  },
+  {
+    slug: "bee-products",
+    name: "Bee Products",
+    image: "/images/categories/bee-products.jpg",
+  },
+  {
+    slug: "nutrition-supplements",
+    name: "Nutritional Supplements",
+    image: "/images/categories/nutrition-supplements.jpg",
+  },
+  {
+    slug: "weight-management",
+    name: "Weight Management & Sports",
+    image: "/images/categories/weight-management.jpg",
+  },
+  {
+    slug: "personal-care",
+    name: "Personal Care",
+    image: "/images/categories/personal-care.jpg",
   },
   {
     slug: "skin-care",
     name: "Skin Care",
     image: "/images/categories/skin-care.jpg",
-  },
-  {
-    slug: "nutrition-supplement",
-    name: "Nutritional Supplements",
-    image: "/images/categories/nutrition-supplements.jpg",
-  },
-
-  {
-    slug: "personal-care",
-    name: "Personal Care",
-    image: "/images/categories/personal-care.jpg",
   },
 ];
